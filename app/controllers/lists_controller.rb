39793af1bd05909154205a37f045072f6b9e3672
Dustin Hall <dustinjonhall@gmail.com>
2022-11-25 16:55:37 +0100

    watch List

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,12 +5,22 @@ class ListsController < ApplicationController
   end
 
   def show
-    @lists = List.find(params[:id])
+    @list = List.find(params[:id])
   end
 
   def new
-    @lists = List.new
+    @list = List.new
   end
 
-  
+  def create
+    @list = List.create(list_params)
+    @list.save
+    redirect_to lists_path
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :bookmark do
-    resources :list
+  resources :lists do
+    resources :bookmarks, except: [:destroy]
   end
+
+  resources :bookmarks, only: [:destroy]
   # Defines the root path route ("/")
   # root "articles#index"
 end
